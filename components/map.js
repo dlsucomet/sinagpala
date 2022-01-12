e342a466b46fb713bf51379bb2844f6613255410
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2022-01-12 14:35:04 +0800

    Added Mapbox geocoder, search api route not working properly

--- components/map.js
+++ components/map.js
@@ -1,5 +1,8 @@
-import { useState } from "react"
+import { useState, useRef, useCallback } from "react"
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import ReactMapGL from "react-map-gl"
+import Geocoder from 'react-map-gl-geocoder'
+import axios from 'axios'
 
 export default function Map() {
     const [viewport, setViewport] = useState({
@@ -9,13 +12,71 @@ export default function Map() {
         longitude: 121.0981465,
         zoom: 20
     });
+    const mapRef = useRef();
+    const handleViewportChange = useCallback(
+        (newViewport) => setViewport(newViewport),
+        []
+      );
+    
+    const handleGeocoderViewportChange = useCallback(
+        (newViewport) => {
+            const geocoderDefaultOverrides = { transitionDuration: 100 };
+
+            return handleViewportChange({
+            ...newViewport,
+            ...geocoderDefaultOverrides
+            });
+        },
+        []
+    );
+
+    function forwardGeocoder(query) {
+        const matchingFeatures = []
+        
+        console.log(query)
+        axios.get(`/api/search/${query}`)
+        .then(response => {
+            console.log(JSON.stringify(response.data))
+        })
+        // for (const feature of customData.features) {
+        //     // Handle queries with different capitalization
+        //     // than the source data by calling toLowerCase().
+        //     if (
+        //     feature.properties.title
+        //     .toLowerCase()
+        //     .includes(query.toLowerCase())
+        //     ) {
+        //         // Add a tree emoji as a prefix for custom
+        //         // data results using carmen geojson format:
+        //         // https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+        //         feature['place_name'] = `🌲 ${feature.properties.title}`;
+        //         feature['center'] = feature.geometry.coordinates;
+        //         feature['place_type'] = ['park'];
+        //         matchingFeatures.push(feature);
+        //     }
+        // }
+        return matchingFeatures;
+    }
     
     return <ReactMapGL
+                ref={mapRef}
                 mapStyle="mapbox://styles/neillua/ckyaxoahl6g4y14o9izyuozem"
                 mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
                 {...viewport}
-                onViewportChange={(nextViewport) => setViewport(nextViewport)}
+                onViewportChange={handleViewportChange}
                 >
-                
+                    <Geocoder
+                        mapRef={mapRef}
+                        zoom={20}
+                        countries={"PH"}
+                        proximity={{
+                            longitude: 121.0981465,
+                            latitude: 14.6335708
+                        }}
+                        localGeocoder={forwardGeocoder}
+                        onViewportChange={handleGeocoderViewportChange}
+                        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
+                        position="top-right"
+                    />
             </ReactMapGL>
 }
