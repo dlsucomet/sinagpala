19aed05d4a0cdda39b93e905323c0bff951113a9
neil matthew lua <neil_lua@dlsu.edu.ph>
2022-01-13 10:45:21 +0800

    Implemented MUI Fix; Added header to map

--- pages/explore.js
+++ pages/explore.js
@@ -1,13 +1,28 @@
 import dynamic from "next/dynamic"
+import Header from "../components/header"
+import NoSsr from "../components/NoSsr"
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles(theme => ({
+    mapContainer: {
+        height: '90vh',
+    },
+}));
 
 export default function Explore(){
+    const classes = useStyles();
+
     const Map = dynamic(() => import("../components/Map"), {
         loading: () => "Loading...",
         ssr: false
     });
 
-    return (
-        <Map />
+    return(
+        <NoSsr>
+            <Header />
+            <div className={classes.mapContainer}>
+                <Map />
+            </div>
+        </NoSsr>
     )
-
 }
