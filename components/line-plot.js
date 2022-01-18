3941e64029034823eeeba6ed7c6677672ec2880f
neil matthew lua <neil_lua@dlsu.edu.ph>
2022-01-18 15:12:37 +0800

    Added notif for no building polygon selected

--- components/line-plot.js
+++ components/line-plot.js
@@ -9,15 +9,18 @@ const useStyles = makeStyles(theme => ({
     },
     plot: {
         width: '100%',
-        height: '100%',
+        height: '450px',
         [theme.breakpoints.up('sm')]: { //https://levelup.gitconnected.com/using-breakpoints-and-media-queries-in-material-ui-47470d3c43d9
             width: '90%',
-            height: '90%',
         },
         [theme.breakpoints.up('md')]: {
             width: '70%',
-            height: '70%',
         },
+    },
+    infoMarker: {
+        fontSize: '30px',
+        padding: '0px',
+        margin: '0px'
     }
 }));
 
@@ -29,26 +32,28 @@ export default function LinePlot(props){
     var dataLabels = [] ;
     const tableTitle = props.type == "month" ? 'Average Monthly kW/h' : 'Average Hourly kW/h';
 
-    if (props.type == "month") {
-        Object.keys(props.data.properties).forEach((key) => {
-            if (key.startsWith("avg_month")) {
-                data.push(props.data.properties[key]);
-            }
-        });
-
-        dataX = [...Array(12).keys()];
-        dataLabels = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-
-    }
-    else if (props.type == "hour") {
-        Object.keys(props.data.properties).forEach((key) => {
-            if (key.startsWith("avg_hour")) {
-                data.push(props.data.properties[key]);
-            }
-        });
-
-        dataX = [...Array(24).keys()];
-        dataLabels = dataX.map((hour) => `${hour}:00`);
+    if (props.data !== null) {
+        if (props.type == "month") {
+            Object.keys(props.data.properties).forEach((key) => {
+                if (key.startsWith("avg_month")) {
+                    data.push(props.data.properties[key]);
+                }
+            });
+    
+            dataX = [...Array(12).keys()];
+            dataLabels = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    
+        }
+        else if (props.type == "hour") {
+            Object.keys(props.data.properties).forEach((key) => {
+                if (key.startsWith("avg_hour")) {
+                    data.push(props.data.properties[key]);
+                }
+            });
+    
+            dataX = [...Array(24).keys()];
+            dataLabels = dataX.map((hour) => `${hour}:00`);
+        }
     }
 
     console.log("LP: ", props.type)
@@ -59,53 +64,61 @@ export default function LinePlot(props){
     // https://plotly.com/javascript/tick-formatting/
     return (
         <div className={classes.container}>
-            <Plot
-                data={[
-                    {
-                        x: dataX,
-                        y: data,
-                        type: 'scatter',
-                        mode: 'lines+marker',
-                        marker: {color: '#fd811e'},
-                        hovertemplate: '%{y}<extra></extra>',
-                    },
-                ]}
-                // style={{ width: "70%", height: "70%" }}
-                className={classes.plot}
-                layout={{
-                  xaxis: {
-                      fixedrange: true,
-                      tickmode: "array", // If "array", the placement of the ticks is set via `tickvals` and the tick text is `ticktext`.
-                      tickvals: dataX,
-                      ticktext: dataLabels,
-                      tickfont: {
-                        // family: 'Old Standard TT, serif',
-                        size: 16,
-                        color: 'black'
-                      },
-                    },
-                  yaxis: {
-                      fixedrange: true,
-                      tickmode: "linear", //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
-                      tick0: 0,
-                      dtick: 5,
-                      tickfont: {
-                        // family: 'Old Standard TT, serif',
-                        size: 16,
-                        color: 'black'
-                      },
-                    },
-                  hovermode: "closest",
-                  hoverlabel: { bgcolor: "#FFF" },
-                  title: tableTitle,
-                  titlefont: {
-                    // family: 'Arial, sans-serif',
-                    size: 20,
-                    color: 'black'
-                  },
-                }}
-                config={{displayModeBar: false, responsive: true }}
-            />
+            {
+                data.length !== 0 ?
+                    <Plot
+                        data={[
+                            {
+                                x: dataX,
+                                y: data,
+                                type: 'scatter',
+                                mode: 'lines+marker',
+                                marker: {color: '#fd811e'},
+                                hovertemplate: '%{y}<extra></extra>',
+                            },
+                        ]}
+                        // style={{ width: "70%", height: "70%" }}
+                        className={classes.plot}
+                        layout={{
+                        xaxis: {
+                            fixedrange: true,
+                            tickmode: "array", // If "array", the placement of the ticks is set via `tickvals` and the tick text is `ticktext`.
+                            tickvals: dataX,
+                            ticktext: dataLabels,
+                            tickfont: {
+                                // family: 'Old Standard TT, serif',
+                                size: 16,
+                                color: 'black'
+                            },
+                            },
+                        yaxis: {
+                            fixedrange: true,
+                            tickmode: "linear", //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
+                            tick0: 0,
+                            dtick: 5,
+                            tickfont: {
+                                // family: 'Old Standard TT, serif',
+                                size: 16,
+                                color: 'black'
+                            },
+                            },
+                        hovermode: "closest",
+                        hoverlabel: { bgcolor: "#FFF" },
+                        title: tableTitle,
+                        titlefont: {
+                            // family: 'Arial, sans-serif',
+                            size: 20,
+                            color: 'black'
+                        },
+                        }}
+                        config={{displayModeBar: false, responsive: true }}
+                    />
+                :
+                    props.type == 'hour' ?
+                            <p className={classes.infoMarker}> Select a building polygon to know more about its potential! </p>
+                    :
+                            <></>
+            }
         </div>
     )
 }
--- pages/explore.js
+++ pages/explore.js
@@ -39,86 +39,6 @@ export default function Explore(){
     const classes = useStyles();
     const [buildingData, setBuildingData] = useState(null);
 
-    // /**
-    //  Everything that follows below are for the Mapbox Map Implementation.
-    //  Technically this was originally in a component Map.js, however we wanted to pass prop change on map click, which triggered state changes. Problem is, the mapbox triggers a rerender with every state change, we only want the card information to change. Using a ref instead of state would stop the rerender, however it will not trigger a state change for the SummaryCard Data. We brought this component out so that it will not have a prop call change.
-
-    //  Original Source for NextJS + Mapbox: https://dev.to/niharikak101/integrating-mapbox-with-next-js-the-cheaper-alternative-to-google-maps-g39
-    //  */
-    // const mapRef = useRef();
-
-    // const maxBounds = {
-    //     south: 121.076,
-    //     west: 14.619,
-    //     north: 121.135,
-    //     east: 14.676
-    // };
-
-    // const [viewport, setViewport] = useState({
-    //     width: "100%",
-    //     height: "100%",
-    //     latitude: 14.6335708,
-    //     longitude: 121.0981465,
-    //     zoom: 18,
-    //     minZoom: 18,
-    //     maxZoom: 20,
-    // });
-    
-    // const onViewportChange = viewport => {
-    //     // console.log(viewport.longitude, viewport.latitude)
-    //     if (viewport.longitude < maxBounds.south) {
-    //         viewport.longitude = maxBounds.south;
-    //     }
-    //     if (viewport.latitude < maxBounds.west) {
-    //         viewport.latitude = maxBounds.west;
-    //     }
-    //     if (viewport.longitude > maxBounds.north) {
-    //         viewport.longitude = maxBounds.north;
-    //     }
-    //     if (viewport.latitude > maxBounds.east) {
-    //         viewport.latitude = maxBounds.east;
-    //     }
-    //     setViewport(viewport);
-    // };
-
-    // const onClick = useCallback(event => {
-    //     const {
-    //       features,
-    //       srcEvent: {offsetX, offsetY}
-    //     } = event;
-    //     console.log(features);
-        
-    //     var mapbuildingData = null;
-
-    //     //Find feature with building properties
-    //     features.forEach(element => {
-    //         if (element.layer.id == "choropleth-fill") {
-    //             mapbuildingData = JSON.parse(JSON.stringify(element));
-    //         }
-    //     });
-
-    //     console.log(mapbuildingData);
-    //     setBuildingData(mapbuildingData);
-    // }, []);
-
-    // useEffect(() => {
-    //     console.log("State Changed!");
-    // }, [buildingData])
-
-    // const refMap = (
-    //     <React.Fragment>
-    //         <ReactMapGL
-    //                 ref={mapRef}
-    //                 mapStyle="mapbox://styles/neillua/ckyaxoahl6g4y14o9izyuozem"
-    //                 mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
-    //                 {...viewport}
-    //                 onViewportChange={nextViewPort => onViewportChange(nextViewPort)}
-    //                 onClick={onClick}
-    //                 >
-    //             </ReactMapGL>
-    //     </React.Fragment>
-    // ) 
-
     const onDataChange = data => {
         setBuildingData(data);
     }
@@ -127,7 +47,6 @@ export default function Explore(){
         <NoSsr>
             <Header />
             <div className={classes.mapContainer}>
-                {/* {refMap} */}
                 <Map onDataChange={onDataChange}/>
                 {
                     buildingData != null ?
@@ -141,21 +60,16 @@ export default function Explore(){
             
             <div className={classes.sideMargin}>
                 <h1>Solar Energy Information</h1>
-                {
-                    buildingData != null ?
-                        <Box sx={{ 
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    p: 1,
-                                    m: 1, 
-                            }}>
-                            <LinePlot data={buildingData}
-                            type="hour"/>
-                            <LinePlot data={buildingData}
-                            type="month"/>
-                        </Box> 
-                    :
-                        <></>
+                 {
+                    <Box sx={{ 
+                                display: 'flex',
+                                flexDirection: 'column',
+                        }}>
+                        <LinePlot data={buildingData}
+                        type="hour"/>
+                        <LinePlot data={buildingData}
+                        type="month"/>
+                    </Box> 
                 }
             </div>
         </NoSsr>
